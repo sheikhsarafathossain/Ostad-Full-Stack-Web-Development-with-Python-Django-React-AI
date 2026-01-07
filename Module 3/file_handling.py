5395bc4e303aaf75c99525d569fefff4f18a3896
Sheikh Sarafat Hossain <sheikhsarafathossain@gmail.com>
2026-01-07 22:59:43 +0600

    Module_3 added

---
+++ Module 3/file_handling.py
@@ -0,0 +1,48 @@
+
+"""
+Docstring for Module 3.file_handling
+
+
+r -> Open file for Reading
+w -> Open file for Writing
+a -> Open file Reading for writing 
+    (writers at the end) if not exist
+    create new
+
+
+"""
+
+
+
+
+"""
+
+f = open("textbook.txt","r")
+data = f.read()
+f.seek(0)
+print(f.read())
+f.close()
+print(data)
+
+with open
+
+"""
+
+
+"""
+
+Mystery of path
+
+Path is where a file / folder resides
+
+two types of path:
+1. Absolute path (starts from os/drive root)
+    E:\code\hobby\text.txt
+2. Relative path (start from current directory)
+    text.txt
+    ..\module 2/text.txt
+
+"""
+
+
+
