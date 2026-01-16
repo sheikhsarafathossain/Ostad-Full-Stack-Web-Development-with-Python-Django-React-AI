8b2580d031e81573810598019f7bc01d89719acd
Sheikh Sarafat Hossain <sheikhsarafathossain@gmail.com>
2026-01-16 21:20:15 +0600

    Class, inheritance, Access Modifier

--- Module 4/habijabi.py
+++ Module 4/habijabi.py
@@ -1,21 +1,75 @@
-class Car:
-    def __init__(self, company_name, model, engine):
-        self.company_name = company_name
-        self.model = model
-        self.engine = engine
+# class Car:
+#     def __init__(self, company_name, model, engine):
+#         self.company_name = company_name
+#         self.model = model
+#         self.engine = engine
 
-    def printAll(self):
-        print(self.company_name)
-        print(self.model)
-        print(self.engine)
+#     def printAll(self):
+#         print(self.company_name)
+#         print(self.model)
+#         print(self.engine)
 
 
-c1 = Car("Toyota", "Corolla", "Petrol Engine")
-c1.printAll()
+# c1 = Car("Toyota", "Corolla", "Petrol Engine")
+# c1.printAll()
 
+# class BankAccount:
+#     def withdraw_money(self):
+#         print("Money withdrawn!")
+
+# class SavingsAccount(BankAccount):
+#     pass
+
+# class CurrentAccount(BankAccount):
+#     pass
+
+# b1 = BankAccount()
+# b1.withdraw_money()
+
+# b1 = SavingsAccount()
+# b1.withdraw_money()
+
+# b1 = CurrentAccount()
+# b1.withdraw_money()
+
+
+# class GrandFather:
+#     def eye(self):
+#         print("Red")
+
+#     def lips(self):
+#         print("hard")
+
+# class Father(GrandFather):
+#     def lips(self):
+#         print("soft")
+
+# class Mother:
+#     def nose(self):
+#         print("pointy")
+#     def lips(self):
+#         print("Hard")
+
+
+# # MRO Method Resolution Order
+
+# class Child(Father): #Multiple Inheritence
+#     def lips(self):
+#         super().lips()
+# c1 = Child()
+# c1.eye()
+# c1.lips()
+
+
+#Access Modifiers
 class BankAccount:
-    def withdraw_money(self):
-        print("Money withdrawn!")
+    def __init__(self):
+        self._name = "Mr A" # Protected
+        self.balance = 12.0 # Public variable
+        self.__pin = 1234 # Private
 
-b1 = BankAccount()
-b1.withdraw_money()
+
+ba = BankAccount()
+print(ba.balance)
+print(ba._name)
+print(ba._BankAccount__pin) # name Mangling
